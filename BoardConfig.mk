a7496641a5c401d8da4a7526eaa88b7642b7b5dd
Adam YH Lee <adam@gumstix.com>
2013-10-28 13:48:34 -0700

    Minimal board file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,49 +1,31 @@
-# Overo configuration
+# Product-specific compile-time definitions
 
+# Build bootloader and kernel from source; don't just install binaries
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_KERNEL := true
-TARGET_PROVIDES_INIT_RC := true
-TARGET_NO_RADIOIMAGE := true
-USE_CAMERA_STUB := true
-#BOARD_USES_ALSA_AUDIO := true
-#BUILD_WITH_ALSA_UTILS := true
-BUILD_WITHOUT_PV := true
-#BUILD_WITH_FULL_STAGEFRIGHT := true
+UBOOT_DIR := $(TOP)/uboot
+UBOOT_CONFIG := omap3_overo
+KERNEL_DIR := $(TOP)/kernel
+KERNEL_CONFIG := overo_android_defconfig
 
-#BUILD_PV_VIDEO_ENCODERS := 1
-
-#BOARD_USES_OMAP3_LIGHTS := true
-
-# Wifi
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-WPA_SUPPLICANT_VERSION      := VER_0_6_X
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/libertas_sdio.ko"
-WIFI_DRIVER_MODULE_NAME := "libertas_sdio"
-WIFI_DRIVER_MODULE_ARG  := ""
-WIFI_TEST_INTERFACE     := "wlan0"
-
-# Bluetooth
-#BOARD_HAVE_BLUETOOTH := true
-
-# Setup some runtime system properties.
+# As of Android 4.3, no special optimizations for Cortex-A8
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_BOARD_PLATFORM := omap3
 TARGET_BOOTLOADER_BOARD_NAME := overo
-TARGET_CPU_ABI  := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
 ARCH_ARM_HAVE_NEON := true
-TARGET_ARCH_VARIANT := armv7-a-neon
 
-TARGET_ARCH:= arm
-TARGET_CPU_VARIANT := generic
-#HARDWARE_OMX := true
+TARGET_PROVIDES_INIT_RC := true
 
-ifdef HARDWARE_OMX
-OMX_JPEG := true
-OMX_VENDOR := ti
-OMX_VENDOR_INCLUDES := \
-   hardware/ti/omx/system/src/openmax_il/omx_core/inc \
-   hardware/ti/omx/image/src/openmax_il/jpeg_enc/inc
-OMX_VENDOR_WRAPPER := TI_OMX_Wrapper
-BOARD_OPENCORE_LIBRARIES := libOMX_Core
-BOARD_OPENCORE_FLAGS := -DHARDWARE_OMX=1
-endif
+# No Recovery Parition
+TARGET_NO_RECOVERY := true
+
+# No cell modem
+TARGET_NO_RADIOIMAGE := true
+
+# Not sute about these settings
+TARGET_USERIMAGES_USE_EXT4 := true
